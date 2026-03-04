3440a4934d43727514a58907398154c137987835
Jonathan Kershenovich <jonkas13@gmail.com>
2026-03-04 10:59:23 -0600

    fix logos

--- app/lib/logos.ts
+++ app/lib/logos.ts
@@ -4,15 +4,15 @@ export function getTeamLogoUrl(sport: Sport, code: string) {
   const c = code.toLowerCase();
 
   if (sport === "nba") {
-    return https://a.espncdn.com/i/teamlogos/nba/500/${c}.png;
+    return "https://a.espncdn.com/i/teamlogos/nba/500/" + c + ".png";
   }
 
   if (sport === "nfl") {
-    return https://a.espncdn.com/i/teamlogos/nfl/500/${c}.png;
+    return "https://a.espncdn.com/i/teamlogos/nfl/500/" + c + ".png";
   }
 
   if (sport === "mlb") {
-    return https://a.espncdn.com/i/teamlogos/mlb/500/${c}.png;
+    return "https://a.espncdn.com/i/teamlogos/mlb/500/" + c + ".png";
   }
 
   return null;
